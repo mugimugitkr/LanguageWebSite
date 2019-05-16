d9926329a5e303923b45c279aa5a14d572629dbb
mugimugitkr <mugimugithirty@gmail.com>
2019-05-16 21:12:29 +0900

    no message

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+$(function() {
+  
+  // 鳥アイコンがクリックされたら
+  $('.bird').click(function() {
+    // ページのトップに戻る
+    $('html').animate({scrollTop: 0}, 1000);
+  })
+
+
+
+  // 画面がスクロールされたら
+  $(window).scroll(function() {
+
+    // スクロールの量を取得
+    let scrollTopValue = $(document).scrollTop();
+
+    if (scrollTopValue > 500) {
+      // スクロールの量が500を超えたら
+      // 鳥アイコンを表示
+      $('.bird').fadeIn();
+    } else {
+      // スクロールの量が500を超えていない場合
+      // 鳥アイコンを非表示
+      $('.bird').fadeOut();
+    }
+  })
+
+})
